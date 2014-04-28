afb98631050d36f1b777d86639e3b61d208d813a
gummiks <gummiks@gmail.com>
2014-04-27 22:45:22 -0400

    lapacke

--- v.c
+++ v.c
@@ -53,7 +53,7 @@ int main(int argc, const char *argv[])
 	//unoptimized_triad(A,B,C);
 	double end_time = omp_get_wtime() - start_time;
 
-	//print_array2d_double('C',C);
+	print_array2d_double('C',C);
 
 	printf("unoptimized base line time: %f\n",end_time);
 
