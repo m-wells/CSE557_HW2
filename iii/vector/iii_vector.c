6a0b1d0cb1b73421287f742cb025f55d167ee5c9
m-wells <maw461@psu.edu>
2014-04-29 23:06:10 -0400

    vectors

--- iii/vector/iii_vector.c
+++ iii/vector/iii_vector.c
@@ -37,9 +37,11 @@ void omp_triad_vec(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SI
 
 			   //v = _mm_set_pd(0,0);
 			   v  = _mm_mul_pd(a,b);
+			   printf("v=%d\n",v);
+
 			   //v  =_mm_add_pd( _mm_mul_pd(a,b),c);
 			   //_mm_store_pd(&my_mult,v);
-			   printf("my_mult %f\n",my_mult);
+			   //printf("my_mult %f\n",my_mult);
 			   //printf("i,j,k %d %d %d\n",i,j,k);
 			   //_mm_store_pd(&C[i][j],c);
 			}
